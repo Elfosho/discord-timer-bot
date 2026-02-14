34ef093e94953072d31e0eda376b9e5dc367ac11
Fosha <hellfosha@gmail.com>
2026-02-14 03:21:24 +0300

    g

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def load_extensions():
 async def main():
     async with bot:
         await load_extensions()
-        await bot.start("TOKEN")
+        await bot.start(os.getenv("TOKEN"))
 
 asyncio.run(main())
 
